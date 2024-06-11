39b28909824d1963f3f96fc9835f4f1fa5a55c20
AlexW29ChemE <103972325+AlexW29ChemE@users.noreply.github.com>
2024-06-12 09:51:16 +1000

    Migrated to MongoDB database

--- index.js
+++ index.js
@@ -1,56 +1,83 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser')
-const dns = require('dns')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose")
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dns = require("dns");
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-//mock database - can replace with mongoose database
-const db = new Map()
+// connect to mongoose
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+  console.log('Connected to Mongo DB: ',process.env.MONGO_URI)
+}).catch((err)=>{
+  console.log('Unable to connect to Mongo, Make sure its running: ',process.env.MONGO_URI)
+})
+
+const urlRecordSchema = mongoose.Schema({
+  url:String, shortUrl:Number
+})
+const URLRecord = mongoose.model('urlRecord',urlRecordSchema);
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-app.use(bodyParser.urlencoded({extended:false}))
+app.use("/public", express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({ extended: false }));
 // logger
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path}`);
   next();
 });
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
-   
-dns.lookup('localhost',(err,address)=>{
-console.log(err,address)
+
+dns.lookup("localhost", (err, address) => {
+  console.log(err, address);
+});
+
+app.post("/api/shorturl", (req, res) => {
+  const { url } = req.body;
+  const [scheme, host, ...rest] = url.split(/:\/\/|[/:]/);
+
+  //validate url
+  dns.lookup(host, (err, address) => {
+    console.log({ url, scheme, host, rest, err, address });
+    if (address) {
+      // check for existing entries
+      URLRecord.findOne({url},(err,data)=>{
+        if(data){
+          res.json({ original_url: url, short_url: data.shortUrl });
+        }else{
+          // find new reference value and create new entry
+          URLRecord.count((err,data)=>{
+            if(err) return res.json({ error: "Database Error" });
+            shortUrl = data+1
+            URLRecord.create({url,shortUrl},(err,data)=>{
+              if(err) return res.json({ error: "Database Error" });
+              res.json({ original_url: url, short_url: shortUrl });
+            })
+          })
+        }
+      })
+    } else {
+      res.json({ error: "invalid url" });
+    }
+  });
+});
+
+app.get("/api/shorturl/:shorturl", (req, res) => {
+
+URLRecord.findOne({shortUrl:req.params.shorturl},(err,data)=>{
+  if(err) return res.status(404).json({error:'Full url not found'})
+    console.log("redirect to ", data.url);
+  res.redirect(data.url);
+
 })
+});
 
-app.post('/api/shorturl',(req,res)=>{
-const {url} = req.body
-const shortUrl = db.size
-const [scheme,host,...rest] = url.split(/:\/\/|[/:]/)
-
-dns.lookup(host,(err,address)=>{
-  console.log({url,scheme, host,rest,err,address})
-  if(address){
-    db.set(shortUrl,url)
-    res.json({original_url:url,short_url:shortUrl})
-  }else{
-    res.json({error:'invalid url'})
-  }
-})
-
-})
-
-app.get('/api/shorturl/:shorturl',(req,res)=>{
-  const fullUrl = db.get(Number(req.params.shorturl))
-console.log('redirect to ',fullUrl)
-res.redirect(fullUrl);
-})
-
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
