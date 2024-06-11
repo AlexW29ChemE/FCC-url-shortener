08957e40c7f3e8adcc8f0480e3fe753c926d6e7f
AlexW29ChemE <103972325+AlexW29ChemE@users.noreply.github.com>
2024-06-11 23:38:28 +1000

    Tests passing - Mock Database used

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser')
+const dns = require('dns')
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+//mock database - can replace with mongoose database
+const db = new Map()
+
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
-
+app.use(bodyParser.urlencoded({extended:false}))
+// logger
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path}`);
+  next();
+});
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+   
+dns.lookup('localhost',(err,address)=>{
+console.log(err,address)
+})
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.post('/api/shorturl',(req,res)=>{
+const {url} = req.body
+const shortUrl = db.size
+const [scheme,host,...rest] = url.split(/:\/\/|[/:]/)
+
+dns.lookup(host,(err,address)=>{
+  console.log({url,scheme, host,rest,err,address})
+  if(address){
+    db.set(shortUrl,url)
+    res.json({original_url:url,short_url:shortUrl})
+  }else{
+    res.json({error:'invalid url'})
+  }
+})
+
+})
+
+app.get('/api/shorturl/:shorturl',(req,res)=>{
+  const fullUrl = db.get(Number(req.params.shorturl))
+console.log('redirect to ',fullUrl)
+res.redirect(fullUrl);
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
